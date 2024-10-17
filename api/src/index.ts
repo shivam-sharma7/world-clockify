36cedb23c2a855c8fad543631929caa893dfe97a
Shivam Sharma <meshivam81@gmail.com>
2024-10-17 15:35:14 +0530

    fix: remove middleware for deplotment

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,5 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { notFound } from './middleware/notFound.error';
 
 import timezoneRoutes from './routes/timezone.routes';
 
@@ -12,9 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to the Timezone API');
+});
+
 app.use('/api/v1', timezoneRoutes);
 
-app.use(notFound);
 app.listen(PORT, () => {
   console.log(`Application is running at http://localhost:${PORT}`);
 });
--- api/src/middleware/notFound.error.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-export const notFound = (req: Request, res: Response, next: any) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-};
